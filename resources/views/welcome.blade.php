aec885e16e271e80d5f7c6d3dc279d4a14dbda3c
Adeyemi Joshua <joshuaadeyemi445@gmail.com>
2024-12-29 19:13:50 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>home</title>
+</head>
+<body>
+
+<a href="{{ route('admin/login') }}">Admin Login</a>
+<a href="{{ route('login') }}">Staff Login</a>
+<a href="{{ route('login') }}">Student Login</a>
+
+</body>
+</html> 
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+//ADMIN
+use App\Http\Controllers\Admin\AdminAuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+//ADMIN 
+Route::get('admin/login', [AdminAuthController::class, 'index'])->name('admin-login');
+Route::post('post/login', [AdminAuthController::class, 'postLogin'])->name('admin-login.post'); 
+
+//AUTH ADMIN
+Route::get('admin/dashboard', [AdminAuthController::class, 'dashboard'])->name('admin-dashboard'); 
+
+Route::get('admin/logout', [AdminAuthController::class, 'logout'])->name('admin-logout');
